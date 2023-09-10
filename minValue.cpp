88dc5cb5e2f0ee9b24c63a25646720e811c17b3e
Kartikgarg2605 <64309017+Kartikgarg2605@users.noreply.github.com>
2023-09-10 23:46:24 +0530

    Add files via upload

---
+++ minValue.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    int min = INT_MAX;
+    for (int i = 0; i < 5; i++)
+    {
+        if (min > arr[i])
+        {
+            min = arr[i];
+        }
+    }
+    cout << min;
+    return 0;
+}
